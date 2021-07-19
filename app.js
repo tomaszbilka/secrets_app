f74bd6f0e6c454103cfd629b795e1c501514914d
Tomasz Biłka <bila4@op.pl>
2021-07-19 12:35:19 +0200

    level 6 login by google

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+require( 'dotenv' ).config();
 const express = require( "express" );
 const ejs = require( "ejs" );
 const mongoose = require( "mongoose" );
 const session = require( "express-session" );
 const passport = require( "passport" );
 const passportLocalMongoose = require( "passport-local-mongoose" );
-
+const GoogleStrategy = require( 'passport-google-oauth20' ).Strategy;
+const findOrCreate = require( "mongoose-findorcreate" );
 
 const app = express();
 
@@ -28,21 +30,56 @@ mongoose.set( 'useCreateIndex', true );
 
 const userSchema = new mongoose.Schema( {
     email: String,
-    password: String
+    password: String,
+    googleId: String
 } );
 
 userSchema.plugin( passportLocalMongoose );
+userSchema.plugin( findOrCreate );
 
 const User = new mongoose.model( "User", userSchema );
 
 passport.use( User.createStrategy() );
 
-passport.serializeUser( User.serializeUser() );
-passport.deserializeUser( User.deserializeUser() );
+passport.serializeUser( function ( user, done ) {
+    done( null, user.id );
+} );
+
+passport.deserializeUser( function ( id, done ) {
+    User.findById( id, function ( err, user ) {
+        done( err, user );
+    } );
+} );
+
+passport.use( new GoogleStrategy( {
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+    function ( accessToken, refreshToken, profile, cb ) {
+        console.log( profile );
+
+        User.findOrCreate( { googleId: profile.id }, function ( err, user ) {
+            return cb( err, user );
+        } );
+    }
+) );
 
 app.get( "/", ( req, res ) => {
     res.render( "home" );
 } );
+
+app.get( '/auth/google',
+    passport.authenticate( 'google', { scope: [ 'profile' ] } )
+);
+
+app.get( '/auth/google/secrets',
+    passport.authenticate( 'google', { failureRedirect: '/login' } ),
+    function ( req, res ) {
+        // Successful authentication, redirect secrect.
+        res.redirect( '/secrets' );
+    } );
 
 app.get( "/login", ( req, res ) => {
     res.render( "login" );
@@ -97,6 +134,7 @@ app.post( "/login", ( req, res ) => {
         }
     } );
 } );
+
 
 
 app.listen( 3000, () => console.log( "Server is running on port 3000" ) );
